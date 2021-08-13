4bf596a161eb6236bfebd0d66f0a946491e62970
Neha Goel <nehagoel66@gmail.com>
2021-08-13 22:42:17 +0530

    changes

--- controllers/login.js
+++ controllers/login.js
@@ -3,24 +3,7 @@ exports.getClients = (req, res, next) => {
     posts: [
       {
         _id: "client1",
-        // clientName: "Neha",
       },
     ],
   });
 };
-
-// exports.createPost = (req, res, next) => {
-//   const title = req.body.title;
-//   const content = req.body.content;
-//   //   console.log(title, content);
-//   console.log("post created");
-
-//   //create post in DB
-//   res.status(201).json({
-//     message: "Post created successfully!",
-//     post: {
-//       _id: new Date().toISOString(),
-//       clientName: clientName,
-//     },
-//   });
-// };
